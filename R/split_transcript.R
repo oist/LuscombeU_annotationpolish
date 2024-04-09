e9ebbb81bb51ed52855e4d0a7d2f4a1bf2e75c01
Johannes Nicolaus Wibisana <johannes.nicolaus@oist.jp>
2024-04-09 15:09:09 +0900

    document package

--- R/split_transcript.R
+++ R/split_transcript.R
@@ -6,6 +6,7 @@
 #' @param liftoff A GenomicRanges object with 'liftoff' gene models to be integrated into the original annotation.
 #' @param cage_peaks A GenomicRanges object containing CAGE peaks used for identifying genes to be split.
 #' @param min_gene_size Integer, minimum gene size (in base pairs) for a gene to be retained. Default is 1000 bp.
+#' @param remove_no_ORF Logical, indicating whether to remove liftoff gene models without a valid ORF. Default is FALSE.
 #'
 #' @return A GenomicRanges object containing the modified gene annotations, with some genes split or replaced based on the overlap with CAGE peaks and the size threshold.
 #' @export
@@ -17,10 +18,15 @@
 #' liftoff <- system.file("extdata", "split_example_liftoff.gff3", package="annotationpolish") |> rtracklayer::import() |> fix_liftoff_oikobase()
 #'
 #' split_transcript(original_annotation, liftoff, cage_peaks)
-split_transcript <- function(original_annotation, liftoff, cage_peaks, min_gene_size = 1000){
+split_transcript <- function(original_annotation, liftoff, cage_peaks, min_gene_size = 1000, remove_no_ORF = F){
 
   # Remove genes smaller than the min_gene_size threshold from the original annotation
   old_annotation <- original_annotation |> filter(gene_id %in% (original_annotation |> filter(type == "gene") |> filter(width >= 1000) %>% .$gene_id))
+
+  # remove liftoff gene models if filtering is TRUE
+  # if remove_no_ORF = T
+  liftoff_orf_genes <- (liftoff |> filter(valid_ORF == "True"))$gene_id |> unique()
+  liftoff <- liftoff |> filter(gene_id %in% liftoff_orf_genes)
 
   # Remove liftoff gene models that do not intersect with the original annotation
   liftoff_annot_overlaps <- plyranges::join_overlap_intersect(liftoff |> filter(type == "gene"),
@@ -55,7 +61,7 @@ split_transcript <- function(original_annotation, liftoff, cage_peaks, min_gene_
   cage_peaks_within_gaps <- join_overlap_inner_directed(cage_peaks, gaps_within_target)
 
   # Find original genes overlapping with CAGE peaks for replacement
-  genes_to_replace <- (original_annotation |> filter(type == "gene"))$gene_id
+  genes_to_replace <- unique((annotation_to_split |> filter(type == "gene"))[subjectHits(findOverlaps(cage_peaks_within_gaps, annotation_to_split |> filter(type == "gene")))]$gene_id)
 
   message(paste(length(genes_to_replace), "Genes to replace", paste(genes_to_replace, collapse = ", ")))
 
@@ -64,15 +70,28 @@ split_transcript <- function(original_annotation, liftoff, cage_peaks, min_gene_
     return(gr)
   }
 
-  message(paste(length(genes_replacement), "New genes", paste(genes_replacement, collapse = ", ")))
-
   genes_replacement <- join_overlap_inner_directed(drop_mcols(original_annotation |> filter(type == "gene", gene_id %in% genes_to_replace)),
                                                    liftoff |> filter(type == "gene"))$gene_id
+
+  message(paste(length(genes_replacement), "New genes", paste(genes_replacement, collapse = ", ")))
+
 
   # Compile the final GenomicRanges object with modified annotations
   final_gr <- c(liftoff |> filter(gene_id %in% genes_replacement),
                 original_annotation |> filter(!gene_id %in% genes_to_replace)) |> sort()
 
+
+  final_gr <- final_gr |> select(-ID) |> add_unique_id()
+
+
+  # unlist Parent and remove_transcript_id if gene
+  mcols(final_gr)$Parent <- sapply(mcols(final_gr)$Parent, function(x) {
+    if (is.null(x) || length(x) == 0) {
+      return(NA)
+    } else {
+      return(x[[1]])
+    }
+  })
 
 
   return(final_gr)
